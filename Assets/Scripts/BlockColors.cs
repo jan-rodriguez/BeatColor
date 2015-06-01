f7f161a80a8a62bf98273e4975a3cd20c7f3b7de
jkr241 <jkr241@mit.edu>
2015-06-01 09:09:58 -0400

    First commit. Got things started

---
+++ Assets/Scripts/BlockColors.cs
@@ -0,0 +1,9 @@
+﻿public enum BlockColors {
+
+//	Blue = 0x2196F3,
+//	Red = 0xF44336,
+//	Green = 0x4CAF50,
+//	Orange = 0xFF9800,
+	Black = 0x000000,
+	White = 0xFFFFFF,
+}
---
+++ Assets/Scripts/ColorSwapper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorSwapper : MonoBehaviour {
+	
+	public float swapTime = 5f;
+	
+	private string[] colorsList;
+	private int currentColorIndex = 0;
+	
+	// Use this for initialization
+	void Start () {
+		colorsList = System.Enum.GetNames (typeof(BlockColors));
+		StartCoroutine (SwapColorEnumerator ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	IEnumerator SwapColorEnumerator () {
+		while (true) {
+			currentColorIndex++;
+			print (currentColorIndex);
+			currentColorIndex %= colorsList.Length;
+			BlockColors curColor = (BlockColors)System.Enum.Parse(typeof(BlockColors), colorsList[currentColorIndex]);
+			Camera.main.backgroundColor = ToColor((int)curColor);
+			yield return new WaitForSeconds(swapTime);
+		}
+	}
+	
+	
+	public Color32 ToColor(int HexVal)
+	{
+		byte R = (byte)((HexVal >> 16) & 0xFF);
+		byte G = (byte)((HexVal >> 8) & 0xFF);
+		byte B = (byte)((HexVal) & 0xFF);
+		return new Color32(R, G, B, 255);
+	}
+}
