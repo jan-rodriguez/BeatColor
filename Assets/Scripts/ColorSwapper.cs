34b8901cc04e2fc032cdec873512613c49126d20
jkr241 <jkr241@mit.edu>
2015-06-02 06:03:33 -0400

    Now disables colors and added music

--- Assets/Scripts/ColorSwapper.cs
+++ Assets/Scripts/ColorSwapper.cs
@@ -2,15 +2,25 @@
 using System.Collections;
 
 public class ColorSwapper : MonoBehaviour {
-	
+
+	public int bpm = 60;
 	public float swapTime = 5f;
+	public bool swapping = true;
 	
 	private string[] colorsList;
 	private int currentColorIndex = 0;
+
+	private BlockColors prevColor;
+
+	private Transform whiteBlocksHolder;
+	private Transform blackBlocksHolder;
 	
 	// Use this for initialization
 	void Start () {
 		colorsList = System.Enum.GetNames (typeof(BlockColors));
+		swapTime = bpm / 60.0f;
+		whiteBlocksHolder = GameObject.Find ("WhiteBlocks").transform;
+		blackBlocksHolder = GameObject.Find ("BlackBlocks").transform;
 		StartCoroutine (SwapColorEnumerator ());
 	}
 	
@@ -21,15 +31,47 @@ public class ColorSwapper : MonoBehaviour {
 
 	//TODO: Probably animte the color changes, also get rid of box colliders for the necessary color
 	IEnumerator SwapColorEnumerator () {
-		while (true) {
+		while (true && swapping) {
+			//Re-enable previous color blocks
+			EnableBlockColliders(prevColor);
+
 			currentColorIndex++;
-			print (currentColorIndex);
 			currentColorIndex %= colorsList.Length;
 			BlockColors curColor = (BlockColors)System.Enum.Parse(typeof(BlockColors), colorsList[currentColorIndex]);
+			prevColor = curColor;
 			Camera.main.backgroundColor = ToColor((int)curColor);
+
+			//Disable current color blocks
+			DisableBlockColliders(curColor);
 			yield return new WaitForSeconds(swapTime);
 		}
 	}
+
+	void EnableBlockColliders (BlockColors color) {
+		ToggleBlockColliders(color, true);
+	}
+
+	void DisableBlockColliders (BlockColors color) {
+		ToggleBlockColliders(color, false);
+	}
+
+	void ToggleBlockColliders (BlockColors color, bool enable) {
+		switch (color) {
+		case BlockColors.Black:
+			for(int i = 0; i < blackBlocksHolder.childCount; i++) {
+				Transform block = blackBlocksHolder.GetChild(i);
+				block.gameObject.SetActive(enable);
+			}
+			break;
+		case BlockColors.White:
+			for(int i = 0; i < whiteBlocksHolder.childCount; i++) {
+				Transform block = whiteBlocksHolder.GetChild(i);
+				block.gameObject.SetActive(enable);
+			}
+			break;
+		}
+	}
+
 	
 	
 	public Color32 ToColor(int HexVal)
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,27 +9,38 @@ public class PlayerControl : MonoBehaviour
 	public bool jump = false;				// Condition for whether the player should jump.
 
 
-	public float moveForce = 365f;			// Amount of force added to move the player left and right.
-	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
-	public float jumpForce = 100f;			// Amount of force added when the player jumps.
+	public float moveForce = 150f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 4f;				// The fastest the player can travel in the x axis.
+	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
 
 
 	private int tauntIndex;					// The index of the taunts array indicating the most recent taunt.
-	private Transform groundCheck;			// A position marking where to check if the player is grounded.
+	private BoxCollider2D groundCheck;			// A position marking where to check if the player is grounded.
+	private BoxCollider2D leftCheck;			// A position marking if the player is running into a wall on the left
+	private BoxCollider2D rightCheck;			// A position marking if the player is running into a wall on the right
+	private bool touchingLeftWall = false;	// Whether or not the player is running into a wall on the left side
+	private bool touchingRightWall = false;	// Whether or not the player is running into a wall on the right side
 	private bool grounded = false;			// Whether or not the player is grounded.
 
 
 	void Awake()
 	{
 		// Setting up references.
-		groundCheck = transform.Find("groundCheck");
+		groundCheck = transform.Find("groundCheck").GetComponent<BoxCollider2D>();
+		leftCheck = transform.Find ("leftCheck").GetComponent<BoxCollider2D>();
+		rightCheck = transform.Find ("rightCheck").GetComponent<BoxCollider2D>();
 	}
 
 
 	void Update()
 	{
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+		// Check if ground collisder is touching the ground
+		grounded = groundCheck.IsTouchingLayers(1 << LayerMask.NameToLayer("Ground"));
+
+		// Check if the side colliders are thouching any walls
+		touchingLeftWall = leftCheck.IsTouchingLayers( 1 << LayerMask.NameToLayer("Ground"));
+		touchingRightWall = rightCheck.IsTouchingLayers ( 1<< LayerMask.NameToLayer ("Ground"));
+
 
 		// If the jump button is pressed and the player is grounded then the player should jump.
 		if (Input.GetButtonDown ("Jump") && grounded) {
@@ -43,29 +54,31 @@ public class PlayerControl : MonoBehaviour
 		// Cache the horizontal input.
 		float h = Input.GetAxis("Horizontal");
 
-		//TODO: Don't allow the user to move horizontally if there's a block on the side
-
 		// The Speed animator parameter is set to the absolute value of the horizontal input.
 //		anim.SetFloat("Speed", Mathf.Abs(h));
 
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
+		if (h * GetComponent<Rigidbody2D> ().velocity.x < maxSpeed) {
 			// ... add a force to the player.
-			GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
-
+			//Make sure the player can't get stuck to a wall
+			if ((h < 0 && !touchingLeftWall) || (h > 0 && !touchingRightWall)) {
+				GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
+			}
+		}
+						
 		// If the player's horizontal velocity is greater than the maxSpeed...
 		if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
 			// ... set the player's velocity to the maxSpeed in the x axis.
 			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
 		// If the input is moving the player right and the player is facing left...
-		if(h > 0 && !facingRight)
-			// ... flip the player.
-			Flip();
-		// Otherwise if the input is moving the player left and the player is facing right...
-		else if(h < 0 && facingRight)
-			// ... flip the player.
-			Flip();
+//		if(h > 0 && !facingRight)
+//			// ... flip the player.
+//			Flip();
+//		// Otherwise if the input is moving the player left and the player is facing right...
+//		else if(h < 0 && facingRight)
+//			// ... flip the player.
+//			Flip();
 
 		// If the player should jump...
 		if(jump)
